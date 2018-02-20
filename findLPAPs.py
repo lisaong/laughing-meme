154af96f015f99e787267d4a45fb29615172ebd2
Ong Hwei-Min Lisa <issohl@nus.edu.sg>
2018-02-20 16:48:37 +0800

    consider size and modified time, sort descending

--- findLPAPs.py
+++ findLPAPs.py
@@ -5,23 +5,31 @@ import collections
 import glob
 import itertools
 import os
+import re
 
 def find_files(path, count, search_count):
     "Finds files from a path"
     d = dict()
+
     for filename in glob.iglob('{}/**/*LPAP*.docx'.format(path), recursive=True):
-        # assumes Windows platform
-        # assumes files modified at the same time will override (don't care for now)
-        d[os.path.getctime(filename)] = filename
+        if not re.search('Archive', filename) and not re.search('LPAP_Sample', filename):
+            ctime = os.path.getctime(filename) # assumes Windows platform
+            size = os.path.getsize(filename)
+            d[ctime * size / 1000] = filename
+            print('{} {}: {}'.format(ctime, size, filename))
         search_count = search_count-1
         if search_count == 0:
             break
-    return collections.OrderedDict(itertools.islice(sorted(d.items(), key=lambda t: t[0]), count))
+    return collections.OrderedDict(\
+        itertools.islice(sorted(d.items(), key=lambda t: t[0], reverse=True), count))
 
-parser = argparse.ArgumentParser(description='Lists LPAP files, sorted by date and size.')
+parser = argparse.ArgumentParser(
+    description='Lists LPAP files, sorted by date and size.')
 parser.add_argument('path', help='folder path to search')
-parser.add_argument('--count', '-c', help='max number of files to return', type=int, default=10)
-parser.add_argument('--search_count', '-s', help='max number of files to search', type=int, default=100)
+parser.add_argument('--count', '-c', help='max number of files to return', type=int,
+    default=10)
+parser.add_argument('--search_count', '-s', help='max number of files to search',
+    type=int, default=100)
 args = parser.parse_args()
 result = find_files(args.path, args.count, args.search_count)
 print(result.items())
