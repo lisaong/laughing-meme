04dc170d84e05a6326e67d36585ff2ede46e9cd8
Ong Hwei-Min Lisa <issohl@nus.edu.sg>
2018-02-28 22:08:07 +0800

    make form look pretty

--- node/tscfinder/index.js
+++ node/tscfinder/index.js
@@ -6,6 +6,8 @@ const app = express()
 app.set('view engine', 'pug')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/assets', express.static('assets'))
+app.use('/images', express.static('images'))
 
 app.get('/', (req, res) => {
     res.render('index', { title: 'TSC Finder', message: 'Enter your description to find TSCs using semantic analysis.' })
