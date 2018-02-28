d9f61c9de89cb62557f17ac0946902905ed62d20
Ong Hwei-Min Lisa <issohl@nus.edu.sg>
2018-02-28 17:48:17 +0800

    get request

--- node/tscfinder/index.js
+++ node/tscfinder/index.js
@@ -4,10 +4,14 @@ const tscFinder = require('./tscs');
 const app = express()
 
 app.get('/', (req, res) => {
-
     let input = "Hello world";
-    let result = await tscFinder.getTopTscMatches(input);
-    res.send();
+    tscFinder.getTopN(input, 5).then(function (result, err) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    });
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
